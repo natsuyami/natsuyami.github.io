d325a794c21b75dfd89047f8df2b8112821d575b
ryan gabriel bunquin <bunquinryan@gmail.com>
2023-01-18 12:30:51 +0800

    add effects and footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-var $loading = $('#flowerLoader').hide(1000);
+var $loading = $('#flowerLoader').hide(500);
 $(document)
 .ajaxStart(function () {
-    $loading.show();
+    $loading.show();    
+    
 })
 .ajaxStop(function () {
     $loading;
+});
+
+$(document).ready(function() { 
+    $('#word1, #word2, #word3, #word4, #word5, #word6').each(function(fadeInDiv) {
+        $(this).delay(fadeInDiv * 1200).fadeIn(1000);
+      });
 });
