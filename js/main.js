9353610e32b5d4803a147fe1ae48e2a4c46233df
Ryan Gabriel Bunquin <bunquinryan@gmail.com>
2023-01-29 16:48:32 +0800

    remove text efffect

--- js/main.js
+++ js/main.js
@@ -7,9 +7,3 @@ $(document)
 .ajaxStop(function () {
     $loading;
 });
-
-$(document).ready(function() { 
-    $('#word1, #word2, #word3, #word4, #word5, #word6').each(function(fadeInDiv) {
-        $(this).delay(fadeInDiv * 1200).fadeIn(1000);
-      });
-});
